1347c4ebc03a8374b16dc0c1a2a60c006363f5cc
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2021-05-17 15:12:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBFirstLibrary;
+using System.Data.Entity;
+namespace EFDemo_DBFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Insert
+            //Category c = new Category();
+            //c.CategoryName = "Breakfast";
+            //c.Description = "Breakfast Items";
+            //NorthwindEntities entities = new NorthwindEntities();
+            //entities.Categories.Add(c);
+            //entities.SaveChanges();
+
+
+            //Update
+            //Category c = new Category();
+            //c.Description = "Idli,Coffee";
+            //NorthwindEntities entities = new NorthwindEntities();
+            //Category founddata=entities.Categories.Find(18);
+            //founddata.Description = c.Description;
+            //entities.SaveChanges();
+
+            //Show all
+            //NorthwindEntities entities = new NorthwindEntities();
+            //DbSet<Category> categorieslist=entities.Categories;
+
+            //foreach (var item in categorieslist)
+            //{
+            //    Console.WriteLine(item.CategoryID);
+            //    Console.WriteLine(item.CategoryName);
+            //    Console.WriteLine(item.Description);
+            //}
+
+            //Delete
+
+            NorthwindEntities entities = new NorthwindEntities();
+            Category founddata = entities.Categories.Find(18);
+            entities.Categories.Remove(founddata);
+            entities.SaveChanges();
+            Console.WriteLine("Deleted");
+
+            Console.Read();
+
+        }
+    }
+}
